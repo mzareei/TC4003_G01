0427a44a9426a75a861ff3fa35ba26f6ace0f21a
Ricardo Benitez <A01018084@itesm.mx>
2020-09-05 16:30:48 -0500

    [Code] Update indentation to spaces assignment 1-1
    Signed-off-by: Ricardo Benitez <A01018084@itesm.mx>

--- assignment1-1/q1.go
+++ assignment1-1/q1.go
@@ -1,6 +1,6 @@
 //Authors: Roberto Julio Saldana Hernandez A00354886
-//		  Juan Antonio Cuellar de la pena A00354849
-//   	  Ricardo Arturo Benitez Cruz A01018084
+//        Juan Antonio Cuellar de la pena A00354849
+//        Ricardo Arturo Benitez Cruz A01018084
 //        Jose Roberto Calderon Samado A00354818
 
 
@@ -8,101 +8,101 @@ package cos418_hw1_1
 //package main
 
 import (
-	"fmt"
-	"sort"
-	"io/ioutil"
-	"log"
-	"strings"
-	"regexp"
+    "fmt"
+    "sort"
+    "io/ioutil"
+    "log"
+    "strings"
+    "regexp"
 )
 
 func main(){
-	topWords("simple.txt",5,2)
+    topWords("simple.txt",5,2)
 }
 
 // Find the top K most common words in a text document.
 // 	path: location of the document
-//	numWords: number of words to return (i.e. k)
-//	charThreshold: character threshold for whether a token qualifies as a word,
-//		e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
+//  numWords: number of words to return (i.e. k)
+//  charThreshold: character threshold for whether a token qualifies as a word,
+//      e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
 // Matching is case insensitive, e.g. "Orange" and "orange" is considered the same word.
 // A word comprises alphanumeric characters only. All punctuations and other characters
 // are removed, e.g. "don't" becomes "dont".
 // You should use `checkError` to handle potential errors.
 func topWords(path string, numWords int, charThreshold int) []WordCount {
-	// TODO: implement me
-	// HINT: You may find the `strings.Fields` and `strings.ToLower` functions helpful
-	// HINT: To keep only alphanumeric characters, use the regex "[^0-9a-zA-Z]+"
+    // TODO: implement me
+    // HINT: You may find the `strings.Fields` and `strings.ToLower` functions helpful
+    // HINT: To keep only alphanumeric characters, use the regex "[^0-9a-zA-Z]+"
 
-	//Return variable
-	var result []WordCount
+    //Return variable
+    var result []WordCount
 
-	//Reading the file 
-	content, err := ioutil.ReadFile(path)
+    //Reading the file 
+    content, err := ioutil.ReadFile(path)
 
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
+    if err != nil {
+        log.Fatal(err)
+        return nil
+    }
 
-	//Removing all punctuations
-	scontent := stripNonAlphanumeric(strings.ToLower(string(content)))
+    //Removing all punctuations
+    scontent := stripNonAlphanumeric(strings.ToLower(string(content)))
 
-	//Using Fields to iterate between words
-	//I am also using ToLower function to convert all content to lowercase
-	words := strings.Fields(scontent)
+    //Using Fields to iterate between words
+    //I am also using ToLower function to convert all content to lowercase
+    words := strings.Fields(scontent)
 
-	//counting words
-	for _, y := range words {
-		
-		if(len(y) >= charThreshold && !wordCounted(result,y)){
-			newWord := WordCount{y,1}
-			result = append(result,newWord)
-		}
-	}
+    //counting words
+    for _, y := range words {
+        
+        if(len(y) >= charThreshold && !wordCounted(result,y)){
+            newWord := WordCount{y,1}
+            result = append(result,newWord)
+        }
+    }
 
-	sortWordCounts(result)
+    sortWordCounts(result)
 
-	return result[0:numWords]
+    return result[0:numWords]
 }
 
 func wordCounted(words []WordCount, word string) bool {
-	for x, y := range words {
-		if(y.Word == word){
-			words[x].Count++
-			return true
-		}
-	}
+    for x, y := range words {
+        if(y.Word == word){
+            words[x].Count++
+            return true
+        }
+    }
 
-	return false
+    return false
 }
 
 func stripNonAlphanumeric(in string) string{
-	//I had to modify a little the regex to exclude space as non-alphanumeric strip
-	reg, _ := regexp.Compile("[^0-9a-zA-Z\\s]+")
-	return reg.ReplaceAllString(in,"")
+    //I had to modify a little the regex to exclude space as non-alphanumeric strip
+    reg, _ := regexp.Compile("[^0-9a-zA-Z\\s]+")
+    return reg.ReplaceAllString(in,"")
 }
 
 // A struct that represents how many times a word is observed in a document
 type WordCount struct {
-	Word  string
-	Count int
+    Word  string
+    Count int
 }
 
 func (wc WordCount) String() string {
-	return fmt.Sprintf("%v: %v", wc.Word, wc.Count)
+    return fmt.Sprintf("%v: %v", wc.Word, wc.Count)
 }
 
 // Helper function to sort a list of word counts in place.
 // This sorts by the count in decreasing order, breaking ties using the word.
 // DO NOT MODIFY THIS FUNCTION!
 func sortWordCounts(wordCounts []WordCount) {
-	sort.Slice(wordCounts, func(i, j int) bool {
-		wc1 := wordCounts[i]
-		wc2 := wordCounts[j]
-		if wc1.Count == wc2.Count {
-			return wc1.Word < wc2.Word
-		}
-		return wc1.Count > wc2.Count
-	})
+    sort.Slice(wordCounts, func(i, j int) bool {
+        wc1 := wordCounts[i]
+        wc2 := wordCounts[j]
+        if wc1.Count == wc2.Count {
+            return wc1.Word < wc2.Word
+        }
+        return wc1.Count > wc2.Count
+    })
 }
--- assignment1-1/q2.go
+++ assignment1-1/q2.go
@@ -1,12 +1,12 @@
 package cos418_hw1_1
 
 import (
-	"bufio"
-	"io"
-	"strconv"
-	//"sync"
-	"os"
-	//"fmt"
+    "bufio"
+    "io"
+    "strconv"
+    //"sync"
+    "os"
+    //"fmt"
 )
 
 //WaitGroup to Sync channels
@@ -16,19 +16,19 @@ import (
 // You should only output to `out` once.
 // Do NOT modify function signature.
 func sumWorker(nums chan int, out chan int) {
-	// TODO: implement me
-	// HINT: use for loop over `nums`
+    // TODO: implement me
+    // HINT: use for loop over `nums`
 
-	//We defef the waitgroup to be done
-	//defer wg.Done()
+    //We defef the waitgroup to be done
+    //defer wg.Done()
 
-	workerTotal := 0//This is a partial sum
-	for n := range nums{
-		workerTotal += n
-	}
+    workerTotal := 0//This is a partial sum
+    for n := range nums{
+        workerTotal += n
+    }
 
-	//We put the partial result on the output channel
-	out <- workerTotal
+    //We put the partial result on the output channel
+    out <- workerTotal
 }
 
 // Read integers from the file `fileName` and return sum of all values.
@@ -37,50 +37,50 @@ func sumWorker(nums chan int, out chan int) {
 // You should use `checkError` to handle potential errors.
 // Do NOT modify function signature.
 func sum(num int, fileName string) int {
-	// TODO: implement me
-	// HINT: use `readInts` and `sumWorkers`
-	// HINT: used buffered channels for splitting numbers between workers
+    // TODO: implement me
+    // HINT: use `readInts` and `sumWorkers`
+    // HINT: used buffered channels for splitting numbers between workers
 
-	//Reading the file 
-	content, e := os.Open(fileName)
-	checkError(e)
-	defer content.Close()
+    //Reading the file 
+    content, e := os.Open(fileName)
+    checkError(e)
+    defer content.Close()
 
-	numbers,e := readInts(content)
-	checkError(e)
-	
-	
-	//fmt.Println(numbers)
+    numbers,e := readInts(content)
+    checkError(e)
+    
+    
+    //fmt.Println(numbers)
 
-	//We need to create the input and output channels that will handle the buffer between jobs
-	//We will create as many channels as workers requested by num parametes
-	inputNums := make(chan int, len(numbers)/num)
-	outputNums := make(chan int, num)//We just need one output channel per worker
+    //We need to create the input and output channels that will handle the buffer between jobs
+    //We will create as many channels as workers requested by num parametes
+    inputNums := make(chan int, len(numbers)/num)
+    outputNums := make(chan int, num)//We just need one output channel per worker
 
-	for i := 0; i < num; i++ {
-		//wg.Add(1)//Add another worker to the WaitGroup
-		go sumWorker(inputNums,outputNums)
-	}
+    for i := 0; i < num; i++ {
+        //wg.Add(1)//Add another worker to the WaitGroup
+        go sumWorker(inputNums,outputNums)
+    }
 
-	for _, n := range numbers{
-		inputNums <- n
-	}
-
-
-	//Concurrency must be over before closing the channels
-	//So I need to wait for the channels before close them
-	//wg.Wait()
-	close(inputNums)
+    for _, n := range numbers{
+        inputNums <- n
+    }
 
 
+    //Concurrency must be over before closing the channels
+    //So I need to wait for the channels before close them
+    //wg.Wait()
+    close(inputNums)
 
-	total := 0
-	for x :=0; x < num; x++ {
-		total += <-outputNums
-	}
-	close(outputNums)
-	
-	return total
+
+
+    total := 0
+    for x :=0; x < num; x++ {
+        total += <-outputNums
+    }
+    close(outputNums)
+    
+    return total
 }
 
 // Read a list of integers separated by whitespace from `r`.
@@ -88,15 +88,15 @@ func sum(num int, fileName string) int {
 // an empty slice of integers and the error that occurred.
 // Do NOT modify this function.
 func readInts(r io.Reader) ([]int, error) {
-	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
-	var elems []int
-	for scanner.Scan() {
-		val, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			return elems, err
-		}
-		elems = append(elems, val)
-	}
-	return elems, nil
+    scanner := bufio.NewScanner(r)
+    scanner.Split(bufio.ScanWords)
+    var elems []int
+    for scanner.Scan() {
+        val, err := strconv.Atoi(scanner.Text())
+        if err != nil {
+            return elems, err
+        }
+        elems = append(elems, val)
+    }
+    return elems, nil
 }
