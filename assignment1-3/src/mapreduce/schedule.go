ff31bd11a31bb4b9b5cb221ef9c32a3e85e8de9b
Ricardo Benitez <A01018084@itesm.mx>
2020-09-06 18:24:37 -0500

    [Code] TestBasic Ok
    Signed-off-by: Ricardo Benitez <A01018084@itesm.mx>

--- assignment1-3/src/mapreduce/schedule.go
+++ assignment1-3/src/mapreduce/schedule.go
@@ -29,32 +29,22 @@ func (mr *Master) schedule(phase jobPhase) {
 
     // Time used to wait for registration of the workers to occur
     time.Sleep(50 * time.Millisecond)
-    // for a, w := range mr.workers {
-    //     debug("a:%v, worker:%v\n", a, w)
-    // }
 
     currentWorkers := len(mr.workers)
-    for i := 0; i < ntasks/(len(mr.workers)); i++ {
+    for i := 0; i < ntasks; i++ {
         var taskArguments DoTaskArgs
-        taskArguments.JobName = fmt.Sprint(i)
+        taskArguments.JobName = mr.jobName
         taskArguments.File = mr.files[i]
         taskArguments.Phase = phase
         taskArguments.TaskNumber = i
         taskArguments.NumOtherPhase = nios
         called := false
-        for currWorker := 0; currWorker < currentWorkers; currWorker++ {
-            called = call(mr.workers[(i + currWorker)%currentWorkers], "Worker.DoTask", taskArguments, new(struct{}))
-            if called == false {
-                fmt.Printf("Error in sending the message to Worker: %v\n", mr.workers[currWorker])
-            }
-        }
+        called = call(mr.workers[i % currentWorkers], "Worker.DoTask", taskArguments, new(struct{}))
 
+        if called == false {
+            fmt.Printf("Error in sending the message to Worker: %v\n", mr.workers[i % currentWorkers])
+        }
     }
-        
-    // mr.Wait()
-    // Call after the tasks have been completed
-    // mr.Wait()
-    debug("RABC was here\n")
 
     debug("Schedule: %v phase done\n", phase)
 }
