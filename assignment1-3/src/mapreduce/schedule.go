09183c6c0ea40a41a9ead23c13a20288c3100e55
Ricardo Benitez <A01018084@itesm.mx>
2020-09-08 17:55:53 -0500

    [Code] Update formatting for mapreduce assignment1-3
    Signed-off-by: Ricardo Benitez <A01018084@itesm.mx>

--- assignment1-3/src/mapreduce/schedule.go
+++ assignment1-3/src/mapreduce/schedule.go
@@ -1,58 +1,59 @@
 package mapreduce
+
 import (
-    "time"
-    "fmt"
+	"fmt"
+	"time"
 )
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
-    var ntasks int
-    var nios int // number of inputs (for reduce) or outputs (for map)
-    switch phase {
-    case mapPhase:
-        ntasks = len(mr.files)
-        nios = mr.nReduce
-    case reducePhase:
-        ntasks = mr.nReduce
-        nios = len(mr.files)
-    }
+	var ntasks int
+	var nios int // number of inputs (for reduce) or outputs (for map)
+	switch phase {
+	case mapPhase:
+		ntasks = len(mr.files)
+		nios = mr.nReduce
+	case reducePhase:
+		ntasks = mr.nReduce
+		nios = len(mr.files)
+	}
 
-    debug("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
+	debug("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
 
-    // All ntasks tasks have to be scheduled on workers, and only once all of
-    // them have been completed successfully should the function return.
-    // Remember that workers may fail, and that any given worker may finish
-    // multiple tasks.
-    //
-    // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-    //
+	// All ntasks tasks have to be scheduled on workers, and only once all of
+	// them have been completed successfully should the function return.
+	// Remember that workers may fail, and that any given worker may finish
+	// multiple tasks.
+	//
+	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+	//
 
-    // Time used to wait for registration of the workers to occur
-    time.Sleep(50 * time.Millisecond)
+	// Time used to wait for registration of the workers to occur
+	time.Sleep(50 * time.Millisecond)
 
-    currentWorker := 0
-    for i := 0; i < ntasks; i++ {
-        // Updates the number of workers on every iteration in case there is a new registration of workers
-        workersSize := len(mr.workers)
+	currentWorker := 0
+	for i := 0; i < ntasks; i++ {
+		// Updates the number of workers on every iteration in case there is a new registration of workers
+		workersSize := len(mr.workers)
 
-        // struct needed by the worker to be used in the RPC
-        var taskArguments DoTaskArgs
-        taskArguments.JobName = mr.jobName
-        taskArguments.File = mr.files[i]
-        taskArguments.Phase = phase
-        taskArguments.TaskNumber = i
-        taskArguments.NumOtherPhase = nios
+		// struct needed by the worker to be used in the RPC
+		var taskArguments DoTaskArgs
+		taskArguments.JobName = mr.jobName
+		taskArguments.File = mr.files[i]
+		taskArguments.Phase = phase
+		taskArguments.TaskNumber = i
+		taskArguments.NumOtherPhase = nios
 
-        // Call to the RPC, if false, there is a timeout of the worker
-        called := call(mr.workers[(i + currentWorker) % workersSize], "Worker.DoTask", taskArguments, new(struct{}))
+		// Call to the RPC, if false, there is a timeout of the worker
+		called := call(mr.workers[(i+currentWorker)%workersSize], "Worker.DoTask", taskArguments, new(struct{}))
 
-        if called == false {
-            fmt.Printf("Error in sending the message to Worker: %v\n", mr.workers[(i + currentWorker) % workersSize])
-            // Try to assign the task to another worker when this assignation fails
-            i -= 1
-        }
-        currentWorker = (currentWorker + 1) % workersSize
-    }
+		if called == false {
+			fmt.Printf("Error in sending the message to Worker: %v\n", mr.workers[(i+currentWorker)%workersSize])
+			// Try to assign the task to another worker when this assignation fails
+			i -= 1
+		}
+		currentWorker = (currentWorker + 1) % workersSize
+	}
 
-    debug("Schedule: %v phase done\n", phase)
+	debug("Schedule: %v phase done\n", phase)
 }
