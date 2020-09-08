b9e9bd7d93147a600ef1ebae4a1d9c7bb7fb3f89
Ricardo Benitez <A01018084@itesm.mx>
2020-09-08 17:39:04 -0500

    [Code] Failure assignment1-3 correct
    Signed-off-by: Ricardo Benitez <A01018084@itesm.mx>

--- assignment1-3/src/mapreduce/schedule.go
+++ assignment1-3/src/mapreduce/schedule.go
@@ -30,20 +30,25 @@ func (mr *Master) schedule(phase jobPhase) {
     // Time used to wait for registration of the workers to occur
     time.Sleep(50 * time.Millisecond)
 
-    workersSize := len(mr.workers)
     currentWorker := 0
     for i := 0; i < ntasks; i++ {
+        // Updates the number of workers on every iteration in case there is a new registration of workers
+        workersSize := len(mr.workers)
+
+        // struct needed by the worker to be used in the RPC
         var taskArguments DoTaskArgs
         taskArguments.JobName = mr.jobName
         taskArguments.File = mr.files[i]
         taskArguments.Phase = phase
         taskArguments.TaskNumber = i
         taskArguments.NumOtherPhase = nios
-        called := false
-        called = call(mr.workers[(i + currentWorker) % workersSize], "Worker.DoTask", taskArguments, new(struct{}))
+
+        // Call to the RPC, if false, there is a timeout of the worker
+        called := call(mr.workers[(i + currentWorker) % workersSize], "Worker.DoTask", taskArguments, new(struct{}))
 
         if called == false {
             fmt.Printf("Error in sending the message to Worker: %v\n", mr.workers[(i + currentWorker) % workersSize])
+            // Try to assign the task to another worker when this assignation fails
             i -= 1
         }
         currentWorker = (currentWorker + 1) % workersSize
